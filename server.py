dfad7492ff95800c89a99e33675ede7762c90854
yimco <yimco@gmail.com>
2024-11-10 20:33:09 -0500

    acplicación para descargar de youtube

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import yt_dlp
-from flask import Flask, request
+from flask import Flask, request, send_file
+import os
 
 app = Flask(__name__)
 
 
-@app.route("/download/song/", methods=["POST"])
+@app.route("/download/", methods=["POST"])
 def download_video():
     url = request.form.get("url")
     preference = request.form.get("preference")
@@ -14,7 +15,7 @@ def download_video():
 
     ydl_opts = {
         "format": "bestaudio/best",
-        "outtmpl": "%(title)s.%(ext)s",
+        "outtmpl": "/tmp/%(title)s.%(ext)s",  # Cambio principal: usar /tmp/
         "postprocessors": [
             {
                 "key": "FFmpegExtractAudio",
@@ -24,11 +25,13 @@ def download_video():
         ],
     }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-    return {"message": "Video descargado con éxito!"}, 200
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            return {"message": "Video descargado con éxito!"}, 200
+    except Exception as e:
+        return {"message": f"Error: {str(e)}"}, 500
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
